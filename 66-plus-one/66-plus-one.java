50e61c186ce36e1f0effea66b4273aecd5e1a0f7
Aryan Gupta <69450553+aryajhs@users.noreply.github.com>
2022-08-29 00:29:50 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (55.01%) - LeetHub

---
+++ 66-plus-one/66-plus-one.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        for(int i=digits.length-1 ;i>=0;i--)
+        {
+            if(digits[i]!=9)
+            {
+            digits[i]=digits[i]+1;
+            
+                return digits;
+            }
+            else
+            {
+                digits[i]=0;
+            }
+        }
+        int arr[] = new int[digits.length+1];
+        arr[0]=1;
+        
+        return arr;
+        
+    }
+}
